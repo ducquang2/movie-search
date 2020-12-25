27e93d5ff8686d8fc47afd5e6297f47c2a065830
ducquang2 <quangbilun@gmail.com>
2020-12-25 12:56:35 +0700

    update

---
+++ src/components/Search.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+
+const Search = (props) => {
+  const [searchValue, setSearchValue] = useState("");
+  
+  const handleSearchInputChanges = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  const resetInputField = () => {
+    setSearchValue("")
+  }
+
+  const SearchFunction = (e) => {
+    e.preventDefault();
+    props.search(searchValue);
+    resetInputField();
+  }
+
+  return (
+      <form className="search">
+        <input
+          value={searchValue}
+          onChange={handleSearchInputChanges}
+          type="text"
+        />
+        <input onClick={SearchFunction} type="submit" value="SEARCH" />
+      </form>
+    );
+}
+
+export default Search;
